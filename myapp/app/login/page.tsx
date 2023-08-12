1f1c3efb45ec2e142b4db637cfab30ef7aa5bc2c
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-08-12 15:02:33 -0400

    update

---
+++ myapp/app/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <section>
+        <h2>Login</h2>
+    </section>
+  )
+}
+
+export default Login
---
+++ myapp/app/register/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const Register = () => {
+  return (
+    <section>
+        <h2>Register</h2>
+    </section>
+  )
+}
+
+export default Register
